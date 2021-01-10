4fe6b671a5ef7d1779a1a421b8021911f1c7a384
Kyle Heestand <hees8253@kettering.edu>
2021-01-10 09:27:15 -0500

    Added more methods and constructors to SetLeds.java

--- src/main/java/frc/robot/commands/leds/SetLeds.java
+++ src/main/java/frc/robot/commands/leds/SetLeds.java
@@ -1,17 +1,39 @@
 package frc.robot.commands.leds;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.enums.LedEnums;
 import frc.robot.subsystems.Led;
 
 public class SetLeds extends CommandBase {
 
-    public static Led ledSubsystem;
+    private final Led ledSubsystem;
+
+    public SetLeds() {
+        this.ledSubsystem = new Led(9);
+        this.ledSubsystem.set(LedEnums.White);
+    }
 
     public SetLeds(LedEnums ledEnumValue) {
-        ledSubsystem = new Led(9);
-        ledSubsystem.set(ledEnumValue);
+        this.ledSubsystem = new Led(9);
+        this.ledSubsystem.set(ledEnumValue);
+    }
+
+    public SetLeds(int pin) {
+        this.ledSubsystem = new Led(pin);
+        this.ledSubsystem.set(LedEnums.White);
+    }
+
+    public SetLeds(int pin, LedEnums ledEnumValue) {
+        this.ledSubsystem = new Led(pin);
+        this.ledSubsystem.set(ledEnumValue);
+    }
+
+    public void setLeds(LedEnums ledEnumValue) {
+        this.ledSubsystem.set(ledEnumValue);
+    }
+
+    public void setLeds(Double ledDoubleValue) {
+        this.ledSubsystem.set(ledDoubleValue);
     }
     
 }
